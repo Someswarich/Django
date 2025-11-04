35437e5b1db5bcf826bd2e8eaaa91b5a39b543b3
Someswarich <someswarichanamala@gmail.com>
2025-11-04 19:33:12 +0530

    crud_opertions

---
+++ CRUD_OPERATIONS/operations/operations_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OperationsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'operations_app'
---
+++ CRUD_OPERATIONS/operations/operations_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name=models.CharField(max_length=30,default='null')
+    description=models.CharField(max_length=50,default='No description')
+    price=models.IntegerField()
+    photo=models.FileField(upload_to='img/')
+
